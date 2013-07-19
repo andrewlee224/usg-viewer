fed1e2f70433274be7fbf844bdfa41d30339348c
Andrzej Lichaczewski <andrewlee224@gmail.com>
2013-07-19 09:40:58 +0200

    Support for different colormaps (Blue & Grayscale ATM)

--- src/com/example/usgviewer/RFImage.java
+++ src/com/example/usgviewer/RFImage.java
@@ -7,18 +7,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.graphics.Bitmap;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.widget.ImageView;
 
 public class RFImage implements TimeViewable {
 
 	private DataInputStream ds;
 	private List<Integer> headers = new ArrayList<Integer>();
-	private short[] frame1d;
+	private int[] intFrame1d;
+	private ColorMap cm = ColorMap.Blue;
 	
 	private int width;
 	private int height;
 	private int frames;
-	private int transmitFreq;
-	private int lineDensity;
 	private int currentFrame;
 
 	public RFImage(File file) {
@@ -34,19 +39,17 @@ public class RFImage implements TimeViewable {
 		width = headers.get(2);
 		height = headers.get(3);
 		frames = headers.get(1);
-		transmitFreq = headers.get(14);
-		lineDensity = headers.get(17);
 		ds.skipBytes(4);
 		currentFrame = 1;
-		//frame = new short[width][height];
-		frame1d = new short[width*height];
+		//frame1d = new short[width*height];
+		intFrame1d = new int[width*height];
 	}
 	
-	public Bitmap getImg() throws IOException {
-		return getAtFrame(9);
+	public Drawable getImg() throws IOException {
+		return getAtFrame(1);
 	}
 	
-	public Bitmap getAtFrame(int prefFrame) throws IOException {
+	public Drawable getAtFrame(int prefFrame) throws IOException {
 		if (prefFrame > frames) return null;
 		
 		int frameDiff = prefFrame - currentFrame;
@@ -54,33 +57,40 @@ public class RFImage implements TimeViewable {
 		currentFrame = prefFrame;
 		
 		for(int i = 0; i < getFrameSize(); i++) {
-			//int col = (int) (i/4);
-			//int row = i%4;
-			
-			//frame[col][row] = Short.reverseBytes(ds.readShort());
-			frame1d[i] = Short.reverseBytes(ds.readShort());
+			//frame1d[i] = Short.reverseBytes(ds.readShort());
+			int val = Short.reverseBytes(ds.readShort());
+			switch(cm) {
+			case Blue:
+				intFrame1d[i] = Math.abs((val/256));
+				break;
+			case Grayscale:
+				intFrame1d[i] = Math.abs(val);
+			}
 		}
 		
 		//convert short[] to int[]
-		int[] intFrame1d = new int[frame1d.length];
 		//byte[] byteFrame1d = new byte[frame1d.length];
 
-		for (int i = 0; i < frame1d.length; i++) {
-			intFrame1d[i] = Math.abs(((frame1d[i]))/256);
-		}
-
 		Bitmap bmp = Bitmap.createBitmap(intFrame1d, height, width, Bitmap.Config.RGB_565);
+		Drawable frameDrawable = new BitmapDrawable(bmp);
 		
-		return bmp;
+		switch(cm) {
+		case Grayscale:
+			frameDrawable = convertToGrayscale(frameDrawable);
+		}
+		return frameDrawable;
 	}
 	
-	public double calcTGC(int frameDepth, int originalValue) {
-		double depthCm = frameDepth/lineDensity;
-		double TGCalpha = 0.25; 	// [dB / MHz*cm]
-		
-		double correctedValue = originalValue * Math.exp(-TGCalpha * transmitFreq * depthCm);
-		
-		return correctedValue;
+	private Drawable convertToGrayscale(Drawable drawable)
+	{
+	    ColorMatrix matrix = new ColorMatrix();
+	    matrix.setSaturation(0);
+
+	    ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
+
+	    drawable.setColorFilter(filter);
+
+	    return drawable;
 	}
 	
 	public int getWidth() {
@@ -102,6 +112,16 @@ public class RFImage implements TimeViewable {
 	public int getCurrentFrame() {
 		return currentFrame;
 	}
+	
+	public void setCurrentFrame(int frame) {
+		currentFrame = frame;
+	}
 
+	public ColorMap getCm() {
+		return cm;
+	}
 
+	public void setCm(ColorMap cm) {
+		this.cm = cm;
+	}
 }
